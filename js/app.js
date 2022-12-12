a10947de35c632a2fff2ca9a4fc7e6a32b540069
Dutch Foy <jdutchfoy@gmail.com>
2022-12-12 00:01:50 -0800

    dayfour

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
 'use strict';
 
+let myForm = document.getelementbyid('myform');
+let tableElem = document.getElementById('sales-table');
+let totalElem = document.getElementsByClassName('totals');
+
+function ramdomCust(min, max){
+
+}
+
+Function Store(a,b,c,d){
+  this.name =a;
+  this.minCust = b;
+  this.maxCust = c;
+  this.avgCookie = d;
+  this.cookiesBrought = [];
+}
 let seattle = new Store('Seattle', 23, 65, 6.3);
 let tokyo = new Store('Tokyo', 3, 24, 1.2);
