2d00d2157b61243cdb0228793ab8ce8c41e54b6c
Dutch Foy <jdutchfoy@gmail.com>
2022-12-13 21:42:48 -0800

    alot more complete css sales index app.js

--- js/app.js
+++ js/app.js
@@ -16,4 +16,9 @@ Function Store(a,b,c,d){
   this.cookiesBrought = [];
 }
 let seattle = new Store('Seattle', 23, 65, 6.3);
-let tokyo = new Store('Tokyo', 3, 24, 1.2);
+let tokyo   = new Store('Tokyo', 3, 24, 1.2);
+let dubai   = new Store('Dubai', 11, 38, 3.7);
+let paris   = new Store('Paris', 20, 38, 2.3);
+let lima    = new Store('Lima', 2, 16, 4.6);
+allStores.push(seattle, tokyo, dubai, paris, lima);
+renderAll();
